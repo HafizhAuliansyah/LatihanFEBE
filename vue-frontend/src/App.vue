49ac6a8a4d6b40df4b3dd12c0c966d797727b8fc
Hazumiran <diazazhari32@gmail.com>
2022-09-24 23:19:02 +0700

    nambahin frontend

---
+++ vue-frontend/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <MyHome msg="Welcome to Your Vue.js App" />
+  <img alt="Vue logo" src="./assets/logo.png" />
+</template>
+
+<script>
+// import HelloWorld from "./components/HelloWorld.vue";
+import MyHome from "./views/MyHome.vue";
+
+export default {
+  name: "App",
+  components: {
+    // HelloWorld,
+    MyHome,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
